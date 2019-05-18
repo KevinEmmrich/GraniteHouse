3d6a9d5e62aaa3e44cf462c006d9b3ed912382d7
Kevin Emmrich <kemmrich@jkt9000.com>
2019-05-18 18:57:57 -0400

    Rroduct Types and Special Tags.

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //GET Create Action Method -- show new productType entry screen
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create Action Method -- get entered data from GET and save to SQL
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));  // or RedirectToAction("Index")
+            }
+            else
+            {
+                return View(productTypes);
+            }
+
+        }
+
+
+        //GET Edit Action Method -- show productType entry screen
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Edit Action Method -- get entered data from GET and save to SQL
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                // updates all fields! need more specific update later on when there are more data columns.
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));  // or RedirectToAction("Index")
+            }
+            else
+            {
+                return View(productTypes);
+            }
+
+        }
+
+        //GET Details Action Method -- show new productType entry screen
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+
+        //GET Delete Action Method -- -- go to confirm page
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Delete Action Method -- get entered data from GET and save to SQL
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));  // or RedirectToAction("Index")
+
+        }
+
+
+
+
+    }  // end class ProductTypesController : Controller
+} // namespace GraniteHouse.Areas.Admin.Controllers
---
+++ GraniteHouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Models
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        /// <summary>
+        /// Constructor to setup dependency injection for ProductTypes
+        /// </summary>
+        /// <param name="db"></param>
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // ******************* Methods  *******************************
+
+        // GET: SpecialTags
+        public ActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+
+        //GET Details Action Method -- show new specialTag entry screen
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var specialTag = await _db.SpecialTags.FindAsync(id);
+            if (specialTag == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialTag);
+        }
+
+        // GET: SpecialTags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SpecialTags/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(specialTags);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(specialTags);
+            }
+
+        }
+
+        // GET: SpecialTags/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var specialTag = await _db.SpecialTags.FindAsync(id);
+            if (specialTag == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialTag);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SpecialTags specialTags)
+        {
+            if (id != specialTags.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                // updates all fields! need more specific update later on when there are more data columns.
+                _db.Update(specialTags);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));  // or RedirectToAction("Index")
+            }
+            else
+            {
+                return View(specialTags);
+            }
+
+        }
+
+        //GET Delete Action Method -- go to confirm page
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var specialTags = await _db.SpecialTags.FindAsync(id);
+            if (specialTags == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialTags);
+        }
+
+        //POST Delete Action Method -- get entered data from GET and save to SQL
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var specialTag = await _db.SpecialTags.FindAsync(id);
+            _db.SpecialTags.Remove(specialTag);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));  // or RedirectToAction("Index")
+
+        }
+    }
+}
